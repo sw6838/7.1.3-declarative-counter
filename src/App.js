7e14ab27ec6c012b281c2a2123b8b0e30571c204
Shabnam Waseem <sw6838@gmail.com>
2022-06-02 12:45:34 -0700

    Updated

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//Import
+import React from 'react';
+import {useState} from 'react';
+//State is Data
+
+//2. Variables and Funtions
+function App() {
+const [counter, setCounter] = useState(5)
+    // 3. JSX
+  return (
+    <div>Hello World</div>
+  )
+}
+
+//4. Export
+export default App
